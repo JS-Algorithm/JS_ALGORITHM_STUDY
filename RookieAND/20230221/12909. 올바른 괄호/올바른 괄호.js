0c26a2710c267b04dc984f7ada34d41ca8e1fce7
RookieAND <gwangin1999@naver.com>
2023-02-16 19:59:30 +0900

    feat: 12909 올바른 괄호 문제 개선된 코드 업로드

--- RookieAND/20230221/12909. 올바른 괄호/올바른 괄호.js
+++ RookieAND/20230221/12909. 올바른 괄호/올바른 괄호.js
@@ -1,4 +1,3 @@
-// '(' 열린 괄호를 만나면 stack 에 이를 추가하고, 닫힌 괄호를 만나면 stack에서 제거한다.
 function solution(s) {
   let openBracket = 0;
 
@@ -25,5 +24,18 @@ function solution(s) {
  * 2. 열린 괄호의 갯수를 판단하는 과정에서, 엄격한 비교 (===) 로 0인지를 판별할 경우에도 실패 처리되었습니다.
  *
  * 이에 대한 해결은 0 이 falsy 한 값임을 캐치해서 조건식을 축약하니 그제서야 통과가 되었습니다.
+ * 하단은 상단의 코드를 보다 짧고 간결하게, 효율적으로 재정리하여 작성한 문제 풀이 코드입니다/
  *
  */
+function solution(s) {
+  let openBracket = 0;
+
+  for (const char of s) {
+    // 열린 괄호인지, 닫힌 괄호인지에 따라 열린 괄호의 수량을 더하거나 빼줌.
+    openBracket += char == '(' ? 1 : -1;
+    // 만약 열린 괄호의 수량이 0 혹은 그 이하로 떨어진다면 false
+    if (openBracket < 0) return false;
+  }
+  // 괄호가 완전히 열리고 닫혀 수량이 0개라면 true, 아니라면 false
+  return openBracket === 0;
+}
