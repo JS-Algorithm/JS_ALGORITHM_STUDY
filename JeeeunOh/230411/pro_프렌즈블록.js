aec13be1fd78167d207a0a075977ac065adde8c2
Jieun Oh <0909oje@naver.com>
2023-04-08 16:53:24 +0900

    feat: 프렌즈블록 풀이

--- JeeeunOh/230411/pro_프렌즈블록.js
+++ JeeeunOh/230411/pro_프렌즈블록.js
@@ -1,25 +1,60 @@
-function solution(survey, choices) {
-    var answer = '';
-    let group = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']];
-    let map = {R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0}
+function solution(m, n, board) {
+    var answer = 0;
+    let d = [[0, 0], [1, 0], [0, 1], [1, 1]];
+    board = board.map((item)=>item.split(''));
+    let flag = true;
     
-    survey.map((item, idx)=>{
-        let sItem = item.split('');
-        let cNum = choices[idx];
-        // 1~4 choice 까지는 0번 인덱스에 -(cNum-4) 만큼 더함
-        // 5~7 choice 까지는 1번 인덱스 점수에 cNum-4 만큼 더함
-        let [id, score] = [cNum-4<0? 0:1, Math.abs(cNum-4)]
-        map[sItem[id]] = map[sItem[id]]+score;
-    })
+    // 현재 위치에서 2x2 블록이 완성되면 
+    // 해당 블록들을 x로 바꾸는 함수
+    const setBoard = (i, j, newBoard) => {
+        for(let k=0 ; k<4; k++){
+            let [cx, cy] = [i+d[k][0], j+d[k][1]];
+            if(board[i][j]!=board[cx][cy]) break;
+            if(k===3){ // 네블록이 모두 같다면
+                for(let z=0 ; z<4 ; z++){
+                    let [cx, cy] = [i+d[z][0], j+d[z][1]];
+                    if(newBoard[cx][cy]!='x'){ // 이미 바뀐 블록 아니라면
+                        flag = false;
+                        newBoard[cx][cy]='x'
+                        answer++;
+                    }
+                }
+            }
+        }
+        return newBoard;
+    }
     
-    // group 내 각 아이템은 사전순으로 정렬되어있음.
-    // -> item[0] 점수 >= item[1] 일 경우 item[0] 을 answer에 추가
-    // 반대면 item[1]을 answer에 추가
-    group.forEach((item)=>{
-        map[item[0]] >= map[item[1]] ?
-            answer+=item[0] : answer+=item[1]
-    })
-
+    while(true){
+        flag = true;
+        let newBoard = board.map(row=>[...row]);
+        
+        for(let i=0 ; i<m-1 ; i++){ // (i, j) 기준 오른쪽옆, 대각선아래, 아래 탐색
+            for(let j=0 ; j<n-1 ; j++){
+                if(board[i][j]==='x') continue;
+                newBoard = setBoard(i, j, newBoard);
+            }
+        }
+        
+        // 옮긴 블록 없으면 break;
+        if(flag) break;
+        
+        let posX = -1;
+        for(let j=0 ; j<n; j++){
+            for(let i=m-1 ; i>=0 ; i--){ // 밑에서부터 x있으면 위에것 끌어오기
+                if(newBoard[i][j] === 'x' && posX === -1){ // 첫 x 지점 알아오기
+                    posX = i;
+                }
+                if(newBoard[i][j]!='x' && posX!=-1){ // x가 아닌데 밑에 x가 있었다면
+                    newBoard[posX][j] = newBoard[i][j]; // 해당 x 위치로 옮기고
+                    newBoard[i][j]='x';
+                    posX--; // 다음 x 위치 한칸 위로 올리기
+                }
+            }
+            posX = -1;
+        }
+        
+        board = newBoard.map(row=>[...row]);
+    }
     
     return answer;
 }
