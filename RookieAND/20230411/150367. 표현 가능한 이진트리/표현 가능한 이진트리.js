69bb8083163704bd6d775f8b908438021ead462b
gwangin1999@naver.com <gwangin1999@naver.com>
2023-04-11 01:51:30 +0900

    feat: 표현 가능한 이진트리 관련 설명 추가

--- RookieAND/20230411/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
+++ RookieAND/20230411/150367. 표현 가능한 이진트리/표현 가능한 이진트리.js
@@ -8,7 +8,7 @@
 
 function solution(numbers) {
   // 포화 이진 트리로서 가능한 문자열의 모든 길이를 구한다.
-  // 10^15의 근삿값은 2^50 이므로, 50개 이내에 적절한 이진트리의 길이를 구할 수 있다.
+  // log2(10^15) = 49.82892142331043, 따라서 근사값인 50을 최대 지수로 놓고 계산한다.
   const possibleBinaryTreeLength = [...Array(50).keys()]
     .slice(1)
     .map((x) => 2 ** x - 1);
@@ -21,7 +21,7 @@ function solution(numbers) {
     for (const binaryLength of possibleBinaryTreeLength) {
       // 포화 이진트리를 이루기 위한 길이를 구했다면, 그만큼 0을 추가해준다.
       if (binaryLength >= currentLength) {
-        binary = binary.padStart(binaryLength, "0");
+        binary = binary.padStart(binaryLength, '0');
         break;
       }
     }
@@ -33,14 +33,14 @@ function solution(numbers) {
   function divideTree(binary) {
     const currentLength = binary.length;
     // 현재 트리의 길이가 1이거나, 전부 0 혹은 1로만 이루어진 경우라면 true
-    const isAllSame = new Set(binary.split("")).size === 1;
+    const isAllSame = new Set(binary.split('')).size === 1;
     if (currentLength === 1 || isAllSame) {
       return true;
     }
 
     const middleIndex = Math.floor(currentLength / 2);
     // 루트 노드가 0이라면 false를 반환해야 한다.
-    if (binary[middleIndex] == "0") return false;
+    if (binary[middleIndex] == '0') return false;
     // 트리의 길이가 1이 아니라면, 또 중간을 나눠서 분할 재귀를 시행한다.
     return (
       divideTree(binary.slice(0, middleIndex)) &&
