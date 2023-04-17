ae2b253598f08de203cf4b719bc5443da3fdc0c1
RookieAND <gwangin1999@naver.com>
2023-04-17 22:13:27 +0900

    feat: 양과 늑대 관련 풀이 최적화

--- RookieAND/20230417/92343. 양과 늑대/양과 늑대.js
+++ RookieAND/20230417/92343. 양과 늑대/양과 늑대.js
@@ -1,7 +1,7 @@
 function solution(info, edges) {
   let answer = 1;
   const length = info.length;
-  const tree = Array.from({ length }, () => []);
+  const tree = Array.from({length}, () => []);
 
   // 간선의 정보를 활용하여 트리를 구축한다.
   for (const [parent, child] of edges) {
@@ -19,15 +19,19 @@ function solution(info, edges) {
     if (sheep === wolves) return;
 
     // 현재 노드로부터 하위 노드로 이동이 가능하다면 다음으로 이동할 노드 목록 가져오기.
-    const connectedNodeList = [...next];
+    let connectedNodeList = [...next];
     const currentIndex = connectedNodeList.indexOf(current);
 
     // 현재 노드와 이어진 모든 노드들을 가져와 다음으로 이동할 경로에 추가한다.
     // 단, 현재 노드는 다음으로 이동할 경로에서 반드시 제거해줘야 한다.
     if (tree[current].length) {
-      connectedNodeList.push(...tree[current]);
+      connectedNodeList = [...connectedNodeList, ...tree[current]];
     }
-    connectedNodeList.splice(currentIndex, 1);
+
+    // 현재 노드를 제외한 나머지만 추출해서 인계 한다.
+    connectedNodeList = connectedNodeList.filter(
+      (_, index) => index !== currentIndex,
+    );
 
     // 현재 노드와 이어진 다음 경로를 향해 재귀 탐색을 진행한다
     for (const connectedNode of connectedNodeList) {
