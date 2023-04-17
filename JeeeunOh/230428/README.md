a36004fbbcf6407671cac68e0979f5117aea8c4d
Jieun Oh <0909oje@naver.com>
2023-04-17 20:51:43 +0900

    feat: 어려운 문제들..

--- JeeeunOh/230428/README.md
+++ JeeeunOh/230428/README.md
@@ -1,8 +1,8 @@
 1. [boj1937번.욕심쟁이 판다](https://www.acmicpc.net/problem/1937)✅
 
-2. [programmers.등산코스 정하기](https://school.programmers.co.kr/learn/courses/30/lessons/118669)
+2. [programmers.등산코스 정하기](https://school.programmers.co.kr/learn/courses/30/lessons/118669)✅
 
-3. [programmers.양과 늑대](https://school.programmers.co.kr/learn/courses/30/lessons/92343)
+3. [programmers.양과 늑대](https://school.programmers.co.kr/learn/courses/30/lessons/92343)✅
 
 4. [programmers.택배상자](https://school.programmers.co.kr/learn/courses/30/lessons/131704)✅
 
---
+++ JeeeunOh/230428/pro_양과늑대.js
@@ -0,0 +1,25 @@
+// 0은 양, 1은 늑대, 연결정보
+function solution(info, edges) {
+    const tree = Array.from({length: info.length}, () => []);
+    for(const [parent, child] of edges){
+        tree[parent].push(child);
+    }
+    
+    let answer=0;
+    
+    const dfs = (node, sheep, wolf, possible) => {
+        answer = Math.max(answer, sheep);
+        if(sheep <= wolf) return;
+        
+        const possibleNode = [...possible, ...tree[node]]; // 이제까지 탐색한 가능한 노드 + 현재 노드 자식 노드
+        possibleNode.splice(possibleNode.indexOf(node), 1); // 현재 탐색 중인 node는 빼줌 (탐색완료)
+        
+        for(const next of possibleNode){
+            if(info[next]) dfs(next, sheep, wolf+1, possibleNode); // 다음 노드가 늑대일 때
+            else dfs(next, sheep+1, wolf, possibleNode); // 다음 노드가 양일 때
+        }
+    }
+    dfs(0, 1, 0, [0]);
+    
+    return answer;
+}
