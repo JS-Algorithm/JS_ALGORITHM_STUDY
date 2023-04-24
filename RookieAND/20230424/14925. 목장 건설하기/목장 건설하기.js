78eb30e54ad0cf38fab37b2c0b4925050e044c14
Gwangin Baik <gwangin1999@naver.com>
2023-04-25 08:24:22 +0900

    chore: 목장 건설하기 주석 추가

--- RookieAND/20230424/14925. 목장 건설하기/목장 건설하기.js
+++ RookieAND/20230424/14925. 목장 건설하기/목장 건설하기.js
@@ -1,5 +1,5 @@
 // 1. 범위가 1000 까지이므로 완전 탐색은 불가능하다고 봐야 한다.
-// 2. 특정 위치를 기준으로 최대 너비가 나왔다면, 메모이제이션을 진행한다.
+// 2. 특정 위치를 기준으로 범주를 1씩 늘려나가면서 새롭게 넓혀진 영역을 탐색.
 
 const input = require("fs")
   .readFileSync("/dev/stdin")
@@ -14,6 +14,7 @@ let answer = 0;
 for (let i = 0; i < M; i++) {
   for (let j = 0; j < N; j++) {
     if (!matrix[i][j]) {
+      // 1×1 을 시작으로 우변과 하변을 체크하는 로직을 반복시킨다.
       let currentLength = 1;
       while (isValidArea(i, j, currentLength)) {
         answer = Math.max(answer, currentLength);
@@ -23,7 +24,6 @@ for (let i = 0; i < M; i++) {
   }
 }
 
-// 예외 처리, M과 N이 1인 경우에는 별도로 처리해주자.
 console.log(answer);
 
 // 현재 좌표를 기준으로, length 만큼 떨어진 곳의 하변, 우변을 모두 체크하는 함수
@@ -38,8 +38,8 @@ function isValidArea(y, x, length) {
     if (matrix[y + length - 1][x + i]) return false;
   }
 
-  // 우변을 이후에 체크한다.
-  for (let j = 0; j < length; j++) {
+  // 우변을 이후에 체크한다. (맨 아래는 이미 체크했기에 패스)
+  for (let j = 0; j < length - 1; j++) {
     if (matrix[y + j][x + length - 1]) return false;
   }
   return true;
