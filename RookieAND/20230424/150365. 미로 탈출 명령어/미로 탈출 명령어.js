9ce7703ac315edd3c193752fd7109f32d4a91ca9
RookieAND <gwangin1999@naver.com>
2023-04-24 23:58:23 +0900

    feat: 미로 탈출 명령어 새로운 풀이 추가

--- RookieAND/20230424/150365. 미로 탈출 명령어/미로 탈출 명령어.js
+++ RookieAND/20230424/150365. 미로 탈출 명령어/미로 탈출 명령어.js
@@ -1,22 +1,15 @@
 function solution(n, m, x, y, r, c, k) {
   let fastAnswer = k - (Math.abs(x - r) + Math.abs(y - c));
-  if (fastAnswer < 0 || fastAnswer % 2 != 0) return "impossible";
+  if (fastAnswer < 0 || fastAnswer % 2 != 0) return 'impossible';
 
-  let d = [
-    [1, 0], // d
-    [0, -1], // l
-    [0, 1], // r
-    [-1, 0], // u
+  let direction = [
+    [1, 0, 'd'],
+    [0, -1, 'l'],
+    [0, 1, 'r'],
+    [-1, 0, 'u'],
   ];
 
-  let str = {
-    0: "d",
-    1: "l",
-    2: "r",
-    3: "u",
-  };
-
-  let answer = "z".repeat(k);
+  let answer = 'z'.repeat(k);
 
   function dfs(L, py, px, sum, dist) {
     if (L > k) return;
@@ -27,26 +20,24 @@ function solution(n, m, x, y, r, c, k) {
         return;
       }
     }
-    if (answer !== "z".repeat(k)) return;
+    if (answer !== 'z'.repeat(k)) return;
 
-    for (let i = 0; i < 4; i++) {
-      const dy = py + d[i][0];
-      const dx = px + d[i][1];
-
-      if (dy <= n && dy > 0 && dx <= m && dx > 0) {
+    for (const [dy, dx, direct] of direction) {
+      const [ny, nx] = [py + dy, px + dx];
+      if (ny <= n && ny > 0 && nx <= m && nx > 0) {
         dfs(
           L + 1,
-          dy,
-          dx,
-          sum + str[i],
-          Math.abs(dy - r) + Math.abs(dx - c) + L + 1
+          ny,
+          nx,
+          sum + direct,
+          Math.abs(ny - r) + Math.abs(nx - c) + L + 1,
         );
       }
     }
   }
-  dfs(0, x, y, "", k);
+  dfs(0, x, y, '', k);
 
-  if (answer === "z".repeat(k)) return "impossible";
+  if (answer === 'z'.repeat(k)) return 'impossible';
 
   return answer;
 }
