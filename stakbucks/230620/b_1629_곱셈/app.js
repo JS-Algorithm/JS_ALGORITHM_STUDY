58e45891e2fb5aa88cc5ffbc16c381b363848339
stakbucks <107744534+stakbucks@users.noreply.github.com>
2023-06-17 23:37:25 +0900

    Update stakbucks/230620/b_1629_곱셈/app.js
    Co-authored-by: 오지은 <0909oje@naver.com>

--- stakbucks/230620/b_1629_곱셈/app.js
+++ stakbucks/230620/b_1629_곱셈/app.js
@@ -7,11 +7,11 @@ function solution(input) {
 
   const power = (a, b) => {
     // 0 제곱하면 1
-    if (b === BigInt(0)) return BigInt(1);
+    if (b === 0n) return BigInt(1);
 
     // a^b=a^(b/2)*a^(b/2) 이용
     const half = power(a, BigInt(parseInt(b / BigInt(2))));
-    if (b % BigInt(2)) {
+    if (b % 2n) {
       // b가 홀수인 경우
       return (half * half * (a % c)) % c;
     }
