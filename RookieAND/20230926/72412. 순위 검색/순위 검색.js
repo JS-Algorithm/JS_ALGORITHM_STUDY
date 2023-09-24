302c0d18d26383d5ca6f782c50e12318b420188e
Baik Gwangin <gwangin1999@naver.com>
2023-09-24 15:48:28 +0900

    feat: 순위 검색 문제 코드 최적화

--- RookieAND/20230926/72412. 순위 검색/순위 검색.js
+++ RookieAND/20230926/72412. 순위 검색/순위 검색.js
@@ -5,31 +5,25 @@ function solution(info, querys) {
     const score = Number(data.pop());
 
     let key = data.join('');
-    hashData.set(
-      key,
-      hashData.has(key) ? [...hashData.get(key), score] : [score],
-    );
+
+    // 점수를 제외한 나머지 조건을 하나의 문자열로 통합하여 key 를 만든다.
+    hashData.set(key, hashData.has(key) ? [...hashData.get(key), score] : [score]);
   });
 
+  // 각 key 를 순회하여 조건에 맞는 점수 목록을 오름차순 정렬한다.
   for (let [key, value] of hashData) {
-    hashData.set(
-      key,
-      value.sort((a, b) => a - b),
-    );
+    hashData.set(key, value.sort());
   }
 
-  const search = (hashData, conditions) => {
+  const search = (query) => {
+    const conditions = query.split(/ and | |-/i).filter((cond) => cond);
     const score = conditions.pop();
     return Array.from(hashData.keys())
-      .filter((key) => conditions.every((v) => key.includes(v)))
-      .reduce(
-        (a, c) =>
-          a +
-          hashData.get(c).slice(binarySearch(hashData.get(c), score)).length,
-        0,
-      );
+      .filter((key) => conditions.every((cond) => key.includes(cond)))
+      .reduce((acc, cur) => acc + hashData.get(cur).slice(binarySearch(hashData.get(cur), score)).length, 0);
   };
 
+  // 점수가 오름차순으로 담긴 배열 내에서 특정 점수를 넘기지 않는 index 값을 추출.
   const binarySearch = (arr, target) => {
     let left = 0;
     let right = arr.length;
@@ -43,8 +37,5 @@ function solution(info, querys) {
     return left;
   };
 
-  return querys.map((e) => {
-    const conditions = e.split(/ and | |-/i).filter((e) => e);
-    return search(hashData, conditions);
-  });
+  return querys.map((query) => search(query));
 }
