b9ace88475e9c8b726392cd51f341d17c0bd8a08
RookieAND <gwangin1999@naver.com>
2023-10-09 23:17:40 +0900

    chore: 문제 풀이 관련 주석 추가

--- RookieAND/20231010/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ RookieAND/20231010/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -9,17 +9,21 @@ function solution(arrayA, arrayB) {
   
   const answer = [];
 
+  // 철수가 가진 카드를 모두 대조하여 최대 공약수를 구한다.
   const gcdA = arrayA.slice(1).reduce((acc, cur) => gcd(acc, cur), arrayA[0]);
+  // 최대 공약수가 1이라면 철수가 가진 패에서 유효한 카드 값이 존재하지 않았다는 의미다.
   if (gcdA > 1) {
       const isAnswered = arrayB.every((num) => num % gcdA !== 0);
       if (isAnswered) answer.push(gcdA);
   }
-    
+   
+  // 영희의 경우에도 마찬가지로 가진 카드를 모두 대조하여 최대 공약수를 구한다.
   const gcdB = arrayB.slice(1).reduce((acc, cur) => gcd(acc, cur), arrayB[0]);
   if (gcdB > 1) {
       const isAnswered = arrayA.every((num) => num % gcdB !== 0);
       if (isAnswered) answer.push(gcdB);
   }
 
+  // 두 케이스 모두 값이 없다면 0을, 그렇지 않다면 더 큰 값을 return 한다.
   return answer.length ? Math.max(...answer) : 0;
 }
